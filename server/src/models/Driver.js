615d3d8a235da665ace0e6eefb0782db12e2d199
SebasN <sebastianniewolski@gmail.com>
2023-10-22 04:43:06 -0300

    3er commit

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const { DataTypes } = require("sequelize");
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('Driver', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Driver",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+      },
+      apellido: {
+        type: DataTypes.STRING, // Agregar campo Apellido
+      },
+      descripcion: {
+        type: DataTypes.TEXT, // Agregar campo Descripción
+      },
+      imagen: {
+        type: DataTypes.STRING,
+        allowNull: true, // Agregar campo Imagen
+      },
+      nacionalidad: {
+        type: DataTypes.STRING, // Agregar campo Nacionalidad
+      },
+      dob: {
+        type: DataTypes.STRING, // Agregar campo Fecha de Nacimiento
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
